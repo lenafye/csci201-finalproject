73c1ebd47b763ff8b6ad84171e269b88f515e65a
Lena Ye <lenaye@usc.edu>
2019-11-19 12:20:10 -0800

    Update Review.java

--- src/Review.java
+++ src/Review.java
@@ -2,58 +2,48 @@ package lenaye_CSCI201L_TrojanEats;
 
 public class Review {
 	private int reviewId;
-	//TODO: decide if necessary to have resId and userId
-	
 	private String username;
 	private int restaurantId;
 	private String restaurantName;
-	private String date;
 	private int rating;
 	private String text;
 	private int score;
 	
-	public Review(int reviewId, String username, int restaurantId, String restaurantName, String date, int rating, String text, int score)
-	{
+	public Review(int reviewId, String username, int restaurantId, String restaurantName, int rating, String text, int score) {
 		this.reviewId = reviewId;
 		this.username = username;
 		this.restaurantId = restaurantId;
 		this.restaurantName = restaurantName;
-		this.date = date;
 		this.rating = rating;
 		this.text = text;
 		this.score = score;
 	}
 	
-	public int getId()
-	{
+	public int getId() {
 		return reviewId;
 	}
-	public String getUsername()
-	{
+	
+	public String getUsername() {
 		return username;
 	}
-	public int getRestaurantId()
-	{
+	
+	public int getRestaurantId() {
 		return restaurantId;
 	}
-	public String getRestaurantName()
-	{
+	
+	public String getRestaurantName() {
 		return restaurantName;
 	}
-	public String getDate()
-	{
-		return date;
-	}
-	public int getRating()
-	{
+	
+	public int getRating() {
 		return rating;
 	}
-	public String getText()
-	{
+	
+	public String getText() {
 		return text;
 	}
-	public int getScore()
-	{
+	
+	public int getScore() {
 		return score;
 	}
 }
