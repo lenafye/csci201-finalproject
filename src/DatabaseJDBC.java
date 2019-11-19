f0b027b2036019e7c5ef71dd687e5c55a417858f
Lena Ye <lenaye@usc.edu>
2019-11-19 12:17:58 -0800

    Update DatabaseJDBC.java

--- src/DatabaseJDBC.java
+++ src/DatabaseJDBC.java
@@ -10,111 +10,87 @@ import java.util.ArrayList;
 public class DatabaseJDBC {
 	
 	public static final long serialVersionUID = 1;
-	private static int currentUserId;
-	private static String currentUsername;
 	
-	public DatabaseJDBC() {
-		currentUserId = 0;
-		currentUsername = "";
-	}
-	
-	public static int getUser()
-	{
-		return currentUserId;
-	}
-	
-	public static String getUsername() {
-		return currentUsername;
-	}
-	
-	public static int register(String username, String password)
-	{
+	public static int register(String username, String password) {
 		Connection conn = null;;
 		ResultSet rs = null;
 		PreparedStatement ps = null;
-		int success = 0;
 		try {
 			conn = DriverManager.getConnection("jdbc:mysql://google/trojaneats?cloudSqlInstance=emunch-csci201-lab7:us-central1:trojaneats201&socketFactory=com.google.cloud.sql.mysql.SocketFactory&useSSL=false&user=emma&password=trojaneats");
 			ps = conn.prepareStatement("SELECT username FROM Users WHERE username=?");
 			ps.setString(1, username);
 			rs = ps.executeQuery();
-			if(!rs.next())
-			{
+			if(!rs.next()) {
 				ps = conn.prepareStatement("INSERT INTO Users (username, password) VALUES (?,?)");
 				ps.setString(1, username);
 				ps.setString(2, password);
 				ps.executeUpdate();
-				success = 1;
-				currentUsername = username;
-				// update currentUserId -- is there a better way than to do another search
+				return 1;
 			}
-		} catch (SQLException sqle)
-		{
+			else {
+				return 0;
+			}
+		} catch (SQLException sqle) {
 			System.out.println(sqle.getMessage());
 		} finally {
 			try {
-				if (rs!=null) { rs.close(); }
-				if (conn!=null) { conn.close(); }
-			} catch (SQLException sqle)
-			{
+				if (rs!=null) {
+					rs.close();
+				}
+				if (conn!=null) {
+					conn.close();
+				}
+			} catch (SQLException sqle) {
 				System.out.println(sqle.getMessage());
 			}
 		}
-		return success;
+		return 0;
 	}
 	
-	public static int login(String username, String password)
-	{
+	public static int login(String username, String password) {
 		Connection conn = null;
 		ResultSet rs = null;
 		PreparedStatement ps = null;
-		int success = -1;
 		try {
+			// Class.forName("com.mysql.jdbc.Driver");
 			conn = DriverManager.getConnection("jdbc:mysql://google/trojaneats?cloudSqlInstance=emunch-csci201-lab7:us-central1:trojaneats201&socketFactory=com.google.cloud.sql.mysql.SocketFactory&useSSL=false&user=emma&password=trojaneats");
 			ps = conn.prepareStatement("SELECT userId FROM Users WHERE username=?");
 			ps.setString(1, username);
 			rs = ps.executeQuery();
 			//no user for that username
-			boolean cont = true;
-			if(!rs.next())
-			{
-				cont = false;
+			if(!rs.next()) {
+				return -1;
 			}
-			ps = conn.prepareStatement("SELECT userId FROM Users WHERE username=? AND password=?");
-			ps.setString(1, username);
-			ps.setString(2, password);
-			if(!rs.next() && cont)
-			{
-				cont = false;
-				success = 0;
+			else {
+				ps = conn.prepareStatement("SELECT * FROM Users WHERE username=? AND password=?");
+				ps.setString(1, username);
+				ps.setString(2, password);
+				if(rs.next()) {
+					return 1;
+				}
+				else {
+					return 0;
+				}
 			}
-			else if(cont){
-				success = 1;
-				currentUserId = rs.getInt("userId");
-				currentUsername = username;
-			}
-		} catch(SQLException sqle)
-		{
+		} catch(SQLException sqle) {
 			System.out.println(sqle.getMessage());
 		} finally {
 			try {
-				if (rs!=null) { rs.close(); }
-				if (conn!=null) { conn.close(); }
-			} catch (SQLException sqle)
-			{
+				if (rs!=null) {
+					rs.close();
+				}
+				if (conn!=null) {
+					conn.close();
+				}
+			} catch (SQLException sqle) {
 				System.out.println(sqle.getMessage());
 			}
 		}
-		return success;
-	}
-	
-	public static void logout() {
-		currentUserId = 0;
+		return 0;
 	}
 	
 	//TODO
-	public static ArrayList<Restaurant> search()
-	{
+	public static ArrayList<Restaurant> search(String input, int choice, String option) {
 		Connection conn = null;
 		PreparedStatement ps = null;
 		ResultSet rs = null;
@@ -122,15 +98,17 @@ public class DatabaseJDBC {
 		try {
 			conn = DriverManager.getConnection("jdbc:mysql://google/trojaneats?cloudSqlInstance=emunch-csci201-lab7:us-central1:trojaneats201&socketFactory=com.google.cloud.sql.mysql.SocketFactory&useSSL=false&user=emma&password=trojaneats");
 			ps = conn.prepareStatement("SELECT * FROM Restaurant");
-		} catch(SQLException sqle)
-		{
+		} catch(SQLException sqle) {
 			System.out.println(sqle.getMessage());
 		} finally {
 			try {
-				if (rs!=null) { rs.close(); }
-				if (conn!=null) { conn.close(); }
-			} catch (SQLException sqle)
-			{
+				if (rs!=null) {
+					rs.close();
+				}
+				if (conn!=null) {
+					conn.close();
+				}
+			} catch (SQLException sqle) {
 				System.out.println(sqle.getMessage());
 			}
 		}
@@ -139,113 +117,133 @@ public class DatabaseJDBC {
 	
 	//returns all reviews for a user
 	//displayed on profile page
-	public static ArrayList<Review> getReviews(String username)
-	{
+	public static ArrayList<Review> getReviews(String username) {
 		Connection conn = null;
 		PreparedStatement ps = null;
+		PreparedStatement ps2 = null;
 		ResultSet rs = null;
+		ResultSet rs2 = null;
 		ArrayList<Review> reviews = new ArrayList<Review>();
 		try {
 			conn = DriverManager.getConnection("jdbc:mysql://google/trojaneats?cloudSqlInstance=emunch-csci201-lab7:us-central1:trojaneats201&socketFactory=com.google.cloud.sql.mysql.SocketFactory&useSSL=false&user=emma&password=trojaneats");
 			ps = conn.prepareStatement("SELECT userId FROM Users WHERE username=?");
 			ps.setString(1, username);
 			rs = ps.executeQuery();
-			rs.next();
-			int userId = rs.getInt("userId");
-			ps = conn.prepareStatement("SELECT * FROM Reviews WHERE userId = ?");
-			while(rs.next())
-			{
-				int reviewId = rs.getInt("reviewId");
-				int restaurantId = rs.getInt("restaurantId");
-				//TODO: check that this is right datatype for sql datetime data type?
-				String date = rs.getString("date");
-				int rating = (int) rs.getFloat("rating");
-				String text = rs.getString("text");
-				int score = rs.getInt("score");
-				ps = conn.prepareStatement("SELECT name FROM Restaurants WHERE restaurantId = ?");
-				ps.setInt(1, restaurantId);
-				rs = ps.executeQuery();
-				rs.next();
-				String restaurantName = rs.getString("name");
-				Review r = new Review(reviewId, username, restaurantId, restaurantName, date, rating, text, score);
-				reviews.add(r);
+			while(rs.next()) {
+				int userId = rs.getInt("userId");
+				ps = conn.prepareStatement("SELECT * FROM Reviews WHERE userId=?");
+				ps.setInt(1, userId);
+				while(rs.next()) {
+					int reviewId = rs.getInt("reviewId");
+					int restaurantId = rs.getInt("restaurantId");
+					int rating = rs.getInt("rating");
+					String text = rs.getString("text");
+					int score = rs.getInt("score");
+					ps2 = conn.prepareStatement("SELECT name FROM Restaurants WHERE restaurantId = ?");
+					ps2.setInt(1, restaurantId);
+					rs2 = ps2.executeQuery();
+					while(rs2.next()) {
+						String restaurantName = rs2.getString("name");
+						Review r = new Review(reviewId, username, restaurantId, restaurantName, rating, text, score);
+						reviews.add(r);
+					}
+				}
 			}
-		} catch(SQLException sqle)
-		{
+		} catch(SQLException sqle) {
 			System.out.println(sqle.getMessage());
 		} finally {
 			try {
-				if (rs!=null) { rs.close(); }
-				if (conn!=null) { conn.close(); }
-			} catch (SQLException sqle)
-			{
+				if (rs!=null) {
+					rs.close();
+				}
+				if(rs2!=null) {
+					rs2.close();
+				}
+				if (conn!=null) {
+					conn.close();
+				}
+			} catch (SQLException sqle) {
 				System.out.println(sqle.getMessage());
 			}
 		}
 		return reviews;
 	}
 	
-	public static void addReview(String restaurantName, String username, int rating, String text)
-	{
+	public static void addReview(String restaurantName, String username, int rating, String text) {
 		Connection conn = null;
 		PreparedStatement ps = null;
 		ResultSet rs = null;
+		ResultSet rs2 = null;
 		try {
 			conn = DriverManager.getConnection("jdbc:mysql://google/trojaneats?cloudSqlInstance=emunch-csci201-lab7:us-central1:trojaneats201&socketFactory=com.google.cloud.sql.mysql.SocketFactory&useSSL=false&user=emma&password=trojaneats");
 			ps = conn.prepareStatement("SELECT restaurantId FROM Restaurants WHERE name=?");
 			ps.setString(1, restaurantName);
 			rs = ps.executeQuery();
-			rs.next();
-			int resId = rs.getInt("restaurantId");
-			ps = conn.prepareStatement("SELECT userId FROM Users WHERE username=?");
-			ps.setString(1, username);
-			rs = ps.executeQuery();
-			rs.next();
-			int userId = rs.getInt("userId");
-			ps = conn.prepareStatement("INSERT INTO Reviews (userId, restaurantId, rating, text, score) VALUES (?,?,?,?,?)");
-			ps.setInt(1, userId);
-			ps.setInt(2, resId);
-			ps.setInt(3, rating);
-			ps.setString(4, text);
-			ps.setInt(4, 0);
-			ps.executeUpdate();
-			
-		} catch (SQLException sqle)
-		{
+			while(rs.next()) {
+				int resId = rs.getInt("restaurantId");
+				ps = conn.prepareStatement("SELECT userId FROM Users WHERE username=?");
+				ps.setString(1, username);
+				rs2 = ps.executeQuery();
+				while(rs2.next()) {
+					int userId = rs2.getInt("userId");
+					ps = conn.prepareStatement("INSERT INTO Reviews (userId, restaurantId, rating, text, score) VALUES (?,?,?,?,?)");
+					ps.setInt(1, userId);
+					ps.setInt(2, resId);
+					ps.setInt(3, rating);
+					ps.setString(4, text);
+					ps.setInt(5, 0);
+					ps.executeUpdate();
+				}
+			}
+		} catch (SQLException sqle) {
 			System.out.println(sqle.getMessage());
 		} finally {
 			try {
-				if (rs!=null) { rs.close(); }
-				if (conn!=null) { conn.close(); }
-			} catch (SQLException sqle)
-			{
+				if (rs!=null) {
+					rs.close();
+				}
+				if(rs2!=null) {
+					rs2.close();
+				}
+				if (conn!=null) {
+					conn.close();
+				}
+			} catch (SQLException sqle) {
 				System.out.println(sqle.getMessage());
 			}
 		}
 	}
 	
-	public static void interactReview(int reviewId, int react)
-	{
+	public static void interactReview(int reviewId, int react) {
 		Connection conn = null;
 		PreparedStatement ps = null;
 		ResultSet rs = null;
+		int score = 0;
 		try {
 			conn = DriverManager.getConnection("jdbc:mysql://google/trojaneats?cloudSqlInstance=emunch-csci201-lab7:us-central1:trojaneats201&socketFactory=com.google.cloud.sql.mysql.SocketFactory&useSSL=false&user=emma&password=trojaneats");
-			//TODO: make sure updates correctly
-			ps = conn.prepareStatement("UPDATE Reviews SET score=score+? WHERE reviewId=?");
-			ps.setInt(1, react);
+			ps = conn.prepareStatement("SELECT * FROM Reviews WHERE reviewId=?");
+			ps.setInt(1, reviewId);
+			rs = ps.executeQuery();
+			while(rs.next()) {
+				score = rs.getInt("score");
+				score += react;
+			}
+			ps = conn.prepareStatement("UPDATE Reviews SET score=? WHERE reviewId=?");
+			ps.setInt(1, score);
 			ps.setInt(2, reviewId);
 			ps.executeUpdate();
 		
-		} catch (SQLException sqle)
-		{
+		} catch (SQLException sqle) {
 			System.out.println(sqle.getMessage());
 		} finally {
 			try {
-				if (rs!=null) { rs.close(); }
-				if (conn!=null) { conn.close(); }
-			} catch (SQLException sqle)
-			{
+				if (rs!=null) {
+					rs.close();
+				}
+				if (conn!=null) {
+					conn.close();
+				}
+			} catch (SQLException sqle) {
 				System.out.println(sqle.getMessage());
 			}
 		}
